818db6fa8b2f1cfb8fde0dd1d3ab86c78507ff62
simonev <simone.vanin@agilebg.com>
2021-07-05 09:17:41 +0200

    Migration scripts

--- l10n_it_vat_registries/migrations/14.0.1.0.2/post-migration.py
+++ l10n_it_vat_registries/migrations/14.0.1.0.2/post-migration.py
@@ -6,15 +6,16 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    if not version:
-        return
+    openupgrade.load_data(
+        env.cr, "l10n_it_vat_registries", "migrations/14.0.1.0.2/noupdate_changes.xml"
+    )
 
     openupgrade.logged_query(
         env.cr,
         """
-update account_tax at
-set at.exclude_from_registries = atc.exclude_from_registries
+update account_tax
+set exclude_from_registries = atc.exclude_from_registries
 from account_tax_code atc
-where at.base_code_id = atc.id;
+where base_code_id = atc.id;
     """,
     )
